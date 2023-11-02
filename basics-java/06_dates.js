3f97911bd00eab9004da30a2a5672385c6a04ae9
TonyStark1103 <110346378+TonyStark1103@users.noreply.github.com>
2023-11-02 19:11:44 +0000

    date

---
+++ basics-java/06_dates.js
@@ -0,0 +1,24 @@
+// Dates 
+
+let date = new Date();
+
+/*
+
+console.log(date);
+// to see the date and understand we have to use the tostring method
+console.log(date.toString())
+
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+//type of the date
+console.log(typeof date);    // obeject would be the answer
+
+*/
+
+let mynewdate = new Date(2023, 0, 23);
+console.log(mynewdate);  //   this will print unreadble format
+
+console.log(mynewdate.toLocaleDateString());
+
+
+
