2688d3900c8d33f2d7f46a654a1f34472cba64c4
TonyStark1103 <110346378+TonyStark1103@users.noreply.github.com>
2023-11-02 19:11:33 +0000

    string and date

---
+++ basics-java/05_strings.js
@@ -0,0 +1,16 @@
+const hero  = "ironman";
+
+console.log(hero);
+console.log(hero.charAt(2))
+console.log(hero.substring(0,3));
+console.log(hero.indexOf('n'));
+
+//slice just make a copy and print it
+console.log(hero.slice(2,4));
+console.log(hero)
+
+//splice is used to cut the desire portion of the string 
+
+console.log(hero.split('man'))
+
+
