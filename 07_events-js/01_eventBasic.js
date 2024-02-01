2f1c0202dfb080afe0e9db7d4ef86685ced030ef
TonyStark1103 <110346378+TonyStark1103@users.noreply.github.com>
2024-02-01 04:58:47 +0000

    update

--- 07_events-js/01_eventBasic.js
+++ 07_events-js/01_eventBasic.js
@@ -1,1 +1,17 @@
 // go to one.html file for events
+
+// two.html source js
+const sayname = function(){
+    console.log("Ashish Garg")
+}
+
+const changeText = function(){
+    document.querySelector('h1').innerHTML = 'THis is a turtorial video'
+}
+setTimeout(sayname, 3000)
+setTimeout(changeText, 5000)
+
+document.getElementById('stop').addEventListener("click", function(){
+        clearTimeout(changeText)
+        console.log("stopped")
+})
